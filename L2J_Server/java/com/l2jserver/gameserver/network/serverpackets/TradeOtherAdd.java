3e3ba6433b6708ffd1ef855f6993c82af97ac408
Sdw- <jeremy.garreau.pro@gmail.com>
2014-11-23 14:48:24 +0100

    Updating TradeOtherAdd

--- L2J_Server/java/com/l2jserver/gameserver/network/serverpackets/TradeOtherAdd.java
+++ L2J_Server/java/com/l2jserver/gameserver/network/serverpackets/TradeOtherAdd.java
@@ -39,6 +39,7 @@ public final class TradeOtherAdd extends AbstractItemPacket
 		writeC(0x1B);
 		
 		writeH(1); // item count
+		
 		writeH(0);
 		writeD(_item.getObjectId());
 		writeD(_item.getItem().getDisplayId());
@@ -46,9 +47,9 @@ public final class TradeOtherAdd extends AbstractItemPacket
 		writeH(_item.getItem().getType2()); // item type2
 		writeH(_item.getCustomType1());
 		
-		writeD(_item.getItem().getBodyPart()); // rev 415 slot 0006-lr.ear 0008-neck 0030-lr.finger 0040-head 0080-?? 0100-l.hand 0200-gloves 0400-chest 0800-pants 1000-feet 2000-?? 4000-r.hand 8000-r.hand
+		writeQ(_item.getItem().getBodyPart()); // rev 415 slot 0006-lr.ear 0008-neck 0030-lr.finger 0040-head 0080-?? 0100-l.hand 0200-gloves 0400-chest 0800-pants 1000-feet 2000-?? 4000-r.hand 8000-r.hand
 		writeH(_item.getEnchant()); // enchant level
-		writeH(0x00);
+		writeH(0x00); // item remodel visual ID
 		writeH(_item.getCustomType2());
 		
 		// T1
