4208a3f722e0cf174d3fb706d991d93cce8329de
Sdw- <jeremy.garreau.pro@gmail.com>
2014-11-29 13:52:53 +0100

    Fixing PartySpelled packet format

--- L2J_Server/java/com/l2jserver/gameserver/network/serverpackets/PartySpelled.java
+++ L2J_Server/java/com/l2jserver/gameserver/network/serverpackets/PartySpelled.java
@@ -52,7 +52,8 @@ public class PartySpelled extends L2GameServerPacket
 			{
 				writeD(info.getSkill().getId());
 				writeH(info.getSkill().getLevel());
-				writeD(info.getTime());
+				writeD(0x00);
+				writeH(info.getTime());
 			}
 		}
 	}
