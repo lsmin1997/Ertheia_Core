ba8a2bcc3d11c86b881a84c9991c508b44e30cc7
Sdw- <jeremy.garreau.pro@gmail.com>
2014-11-29 13:57:39 +0100

    Fixing Trade packet format

--- L2J_Server/java/com/l2jserver/gameserver/network/serverpackets/TradeOtherAdd.java
+++ L2J_Server/java/com/l2jserver/gameserver/network/serverpackets/TradeOtherAdd.java
@@ -18,7 +18,6 @@
  */
 package com.l2jserver.gameserver.network.serverpackets;
 
-import com.l2jserver.gameserver.model.ItemInfo;
 import com.l2jserver.gameserver.model.TradeItem;
 
 /**
@@ -39,20 +38,7 @@ public final class TradeOtherAdd extends AbstractItemPacket
 		writeC(0x1B);
 		
 		writeH(1); // item count
-		
 		writeH(0);
-		writeD(_item.getObjectId());
-		writeD(_item.getItem().getDisplayId());
-		writeQ(_item.getCount());
-		writeH(_item.getItem().getType2()); // item type2
-		writeH(_item.getCustomType1());
-		
-		writeQ(_item.getItem().getBodyPart()); // rev 415 slot 0006-lr.ear 0008-neck 0030-lr.finger 0040-head 0080-?? 0100-l.hand 0200-gloves 0400-chest 0800-pants 1000-feet 2000-?? 4000-r.hand 8000-r.hand
-		writeH(_item.getEnchant()); // enchant level
-		writeH(0x00); // item remodel visual ID
-		writeH(_item.getCustomType2());
-		
-		// T1
-		writeItemElementalAndEnchant(new ItemInfo(_item));
+		writeItem(_item);
 	}
 }
