464f067ea4349cccdbebf6775161f02ec1ce4e00
Sdw- <jeremy.garreau.pro@gmail.com>
2015-01-09 21:54:59 +0100

    Adding ExLoginVitalityEffectInfo packet

--- L2J_Server/java/com/l2jserver/gameserver/network/serverpackets/ExLoginVitalityEffectInfo.java
+++ L2J_Server/java/com/l2jserver/gameserver/network/serverpackets/ExLoginVitalityEffectInfo.java
@@ -1,52 +1,53 @@
 /*
  * Copyright (C) 2004-2015 L2J Server
- * 
+ *
  * This file is part of L2J Server.
- * 
+ *
  * L2J Server is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * L2J Server is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.l2jserver.gameserver.network.clientpackets;
+package com.l2jserver.gameserver.network.serverpackets;
 
+import com.l2jserver.Config;
+import com.l2jserver.gameserver.model.actor.stat.PcStat;
+import com.l2jserver.gameserver.model.variables.AccountVariables;
 import com.l2jserver.gameserver.network.L2GameClient;
-import com.l2jserver.gameserver.network.serverpackets.CharSelectionInfo;
-import com.l2jserver.gameserver.network.serverpackets.ExLoginVitalityEffectInfo;
 
 /**
- * (ch)
- * @author KenM
+ * @author Sdw
  */
-public class RequestGotoLobby extends L2GameClientPacket
+public class ExLoginVitalityEffectInfo extends L2GameServerPacket
 {
-	private static final String _C__D0_38_REQUESTGOTOLOBBY = "[C] D0:38 RequestGotoLobby";
+	private final int _points;
 	
-	@Override
-	protected void readImpl()
+	public ExLoginVitalityEffectInfo(L2GameClient client)
 	{
-		// trigger
+		final AccountVariables vars = new AccountVariables(client.getAccountName());
+		_points = vars.getInt(PcStat.VITALITY_VARIABLE, Config.STARTING_VITALITY_POINTS);
 	}
 	
 	@Override
-	protected void runImpl()
+	protected final void writeImpl()
 	{
-		L2GameClient client = getClient();
-		client.sendPacket(new ExLoginVitalityEffectInfo(client));
-		client.sendPacket(new CharSelectionInfo(client.getAccountName(), client.getSessionId().playOkID1));
-	}
-	
-	@Override
-	public String getType()
-	{
-		return _C__D0_38_REQUESTGOTOLOBBY;
+		writeC(0xFE);
+		writeH(0x119);
+		
+		writeD((int) (Config.RATE_VITALITY_EXP_MULTIPLIER * 100));
+		writeD(5); // Remaining item counts
+		
+		writeD(_points);
+		writeD(0);
+		writeD(0);
+		writeD(0);
 	}
 }
